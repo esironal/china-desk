e19b491263d8bb4e75a717e78e2b735338bc231b
Nuage <https://nuage.kerjean.me>
2018-09-28 04:27:26 +0000

    save (shortcut1.js): /js/shortcut1.js

--- js/shortcut1.js
+++ js/shortcut1.js
@@ -1,11 +1,12 @@
-﻿//id,iconName,iconUrl,url,width,height
+//id,iconName,iconUrl,url,width,height
 var shortcut = [
 [0,"SimpleNote","images/my-app-icons/Simplenote.png","https://esironal.github.io/apps/simple/",750,330],
 [1,"Linux Js","images/my-app-icons/utilities terminal.png","https://esironal.github.io/Html-Terminal/demos/simple.html",800,500],
 [2,"Raindrop","images/my-app-icons/raindrop-icon-128.png","https://raindrop.io/app",750,330],
 [3,"Leanote","images/my-app-icons/leanote.png","https://leanote.com/note",750,330],
-[4,"Bitnami","images/my-app-icons/bitnami.png","https://bitnami.com/stacks",750,330],
+[4,"OS.js","images/my-app-icons/bitnami.png","https://demo.os-js.org/",750,330],
 [5,"CodeSandBox","images/my-app-icons/codesandbox.png","https://codesandbox.io",750,330],
 [6,"PHP Fiddle","images/my-app-icons/seo-web-code-icon.png","https://phpfiddle.org/",750,330],
-[7,"Encode-Decode","images/my-app-icons/encode.png","apps/encode-decode.html",800,320]
+[7,"Encode-Decode","images/my-app-icons/encode.png","apps/encode-decode.html",800,320],
+[8,"Encode-Decode","https://raw.githubusercontent.com/mickael-kerjean/nuage/master/.assets/img/logo.png","https://nuage.kerjean.me",800,320]
 ];
