b79d3774b3ab0ef64657d85f8fa66e71d0372026
Nuage <https://nuage.kerjean.me>
2018-09-28 22:14:50 +0000

    save (shortcut2.js): /js/shortcut2.js

--- js/shortcut2.js
+++ js/shortcut2.js
@@ -3,4 +3,4 @@ var shortcut = [
 [0,"PHP Uniter","images/i/icon21.png","https://uniter.github.io/",800,400],
 [1,"php Fiddle Lite","images/i/icon22.png","https://phpfiddle.org/lite",750,340],
 [2,"Electron Apps","images/my-app-icons/electron-icon.png","https://electronjs.org/apps",750,400]
-]
+];
