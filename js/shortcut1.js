52db8308591aa1ffc09b4c6a8d52fc5a5aec840b
Nuage <https://nuage.kerjean.me>
2018-09-29 03:20:31 +0000

    save (shortcut1.js): /js/shortcut1.js

--- js/shortcut1.js
+++ js/shortcut1.js
@@ -1,12 +1,13 @@
 //id,iconName,iconUrl,url,width,height
 var shortcut = [
-[0,"SimpleNote","images/my-app-icons/Simplenote.png","https://esironal.github.io/apps/simple/",750,330],
-[1,"Linux Js","images/my-app-icons/utilities terminal.png","https://esironal.github.io/Html-Terminal/demos/simple.html",800,500],
-[2,"Raindrop","images/my-app-icons/raindrop-icon-128.png","https://raindrop.io/app",750,330],
-[3,"Leanote","images/my-app-icons/leanote.png","https://leanote.com/note",750,330],
+[0,"SimpleNote","images/my-app-icons/Simplenote.png","https://esironal.github.io/apps/simple/",800,500],
+[1,"Linux Js","images/my-app-icons/utilities terminal.png","https://esironal.github.io/Html-Terminal/demos/simple.html",800,600],
+[2,"Raindrop","images/my-app-icons/raindrop-icon-128.png","https://raindrop.io/app",800,600],
+[3,"Leanote","images/my-app-icons/leanote.png","https://leanote.com/note",800,600],
 [4,"OS.js","images/my-app-icons/bitnami.png","https://demo.os-js.org/",750,330],
 [5,"CodeSandBox","images/my-app-icons/codesandbox.png","https://codesandbox.io",750,330],
 [6,"PHP Fiddle","images/my-app-icons/seo-web-code-icon.png","https://phpfiddle.org/",750,330],
 [7,"Encode-Decode","images/my-app-icons/encode.png","apps/encode-decode.html",800,320],
-[8,"cors-anywhere","https://avatars0.githubusercontent.com/u/1365071?s=128&v=4","https://robwu.nl/cors-anywhere.html",800,320]
+[8,"cors-anywhere","https://avatars0.githubusercontent.com/u/1365071?s=128&v=4","https://robwu.nl/cors-anywhere.html",800,600],
+[9,"Ast Explorer","https://astexplorer.net/favicon.png","https://astexplorer.net",800,600]
 ];
